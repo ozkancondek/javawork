7e06c7145e0c6ce3bf7cd0ff6f5e1cca2a0028da
ozkancondek <ozkncndek@gmail.com>
2024-01-14 21:54:38 +0100

    Java Introduction

---
+++ Udemy_01/src/Introduction/fileSystem.java
@@ -0,0 +1,23 @@
+package Introduction;
+// The public keyword is an access modifier used for classes, attributes, methods and constructors
+
+//making the accessible by any other class
+
+public class fileSystem {
+    // This is the main function. When project run, this function works first
+    // Static means, static methods/attributes can be accessing without creating an
+    // object
+    // void, return type of the function. In this case it returns nothing also means
+    // without return
+    // parameters, u can send values in String tye
+    public static void main(String[] args) {
+        System.out.println("Hello ozkan");
+    }
+
+}
+
+
+/* ***************************Notes********************
+*Creating package is under src file and adding file means adding new class
+*Each class has a spesific package
+*/
