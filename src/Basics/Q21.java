bd3b84860b6a2d916fcd79dc712e58b166886e33
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-02-20 09:33:58 +0100

    Add files via upload
    String contains

---
+++ src/Basics/Q21.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class Q21 {
+		public static boolean isContains (String str1,String str2) {
+			return str1.contains(str2);
+		}
+}
+
+
+/*
+ ******* Instructions*********** 
+171. Write a Java program to accept two strings and test if the second string contains the first one.
+
+Input first string: Once in a blue moon
+Input second string: See eye to eye
+If the second string contains the first one? false
+ */
