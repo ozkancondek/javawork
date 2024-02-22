669da18c525d062731be23c274797b21d3406de3
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-02-22 16:11:40 +0100

    Add files via upload
    time function in java

---
+++ src/Basics/Q24.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class Q24 {
+	
+	public static void dateFinder() {
+		  java.util.Date date = new java.util.Date();    
+		    System.out.println(date);  
+ 
+	}
+
+	public static void main(String[] argvs) {
+		dateFinder();
+	}
+}
