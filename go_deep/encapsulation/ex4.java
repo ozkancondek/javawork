9cc2e967d767dedcad446ddd5da25559462f49e3
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-12-06 10:17:36 +0100

    Create ex4.java

---
+++ go_deep/encapsulation/ex4.java
@@ -0,0 +1,28 @@
+/*
+Write a Java program to create an interface called Greeting with a method sayHello().
+In the main method, create an anonymous class that implements the Greeting interface 
+and override the sayHello() method to print "Hello, World!". Call the sayHello() method..
+*/
+
+//create interface
+
+ interface Greeting{
+    public void sayHello();
+}
+
+// Main class containing the main method
+public class Main {
+    public static void main(String[] args) {
+        // Creating an anonymous class that implements the Greeting interface
+        Greeting greeting = new Greeting() {
+            @Override
+            public void sayHello() {
+                // Overriding the sayHello method to print "Hello, World!"
+                System.out.println("Hello, World!");
+            }
+        };
+
+        // Calling the sayHello method of the anonymous class
+        greeting.sayHello();
+    }
+}
