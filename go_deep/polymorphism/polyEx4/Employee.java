8a9d3c0904292d89980062c37b73eff52775d495
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2025-01-14 09:55:30 +0100

    Update Employee.java

--- go_deep/polymorphism/polyEx4/Employee.java
+++ go_deep/polymorphism/polyEx4/Employee.java
@@ -1,3 +1,7 @@
+/*
+Write a Java program to create a class Employee with a method called calculateSalary(). Create two subclasses Manager and Programmer. In each subclass, override the calculateSalary() method to calculate and return the salary based on their specific roles.
+*/
+
 package com.example.demo.polyEx;
 
 public class Employee {
