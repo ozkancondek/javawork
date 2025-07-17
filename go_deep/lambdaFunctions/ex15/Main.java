b34b2d1f4ef792e1e488bbc485b55e867864b5d8
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2025-07-17 08:04:53 +0200

    Add files via upload

---
+++ go_deep/lambdaFunctions/ex15/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    // Create a list of integers
+    List < Integer > nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    System.out.println("Original list elements: " + nums);
+    // Calculate the sum of squares of odd numbers using lambda expression
+    int sum_squares_odd = nums.stream()
+      .filter(n -> n % 2 != 0)
+      .mapToInt(n -> n * n)
+      .sum();
+
+    // Calculate the sum of squares of even numbers using lambda expression
+    int sum_squares_even = nums.stream()
+      .filter(n -> n % 2 == 0)
+      .mapToInt(n -> n * n)
+      .sum();
+
+    // Print the results
+    System.out.println("\nSum of squares of odd numbers: " + sum_squares_odd);
+    System.out.println("\nSum of squares of even numbers: " + sum_squares_even);
+  }
+}
