8807a4dd7dc76c4bb820a34a7a3264bae4be2c81
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2025-08-19 14:02:28 +0200

    Add files via upload

---
+++ go_deep/lambdaFunctions/ex18/Main.java
@@ -0,0 +1,26 @@
+package com.example.github.lambdaFunctions.ex18;
+
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        // Define the perfect square check lambda expression
+        Predicate < Integer > isPerfectSquare = n -> {
+            int sqrt = (int) Math.sqrt(n);
+            return sqrt * sqrt == n;
+        };
+
+        // Test the lambda expression with some numbers
+        int N = 36;
+        boolean result1 = isPerfectSquare.test(N);
+        System.out.println(N + " is a perfect square? " + result1);
+
+        N = 26;
+        boolean result2 = isPerfectSquare.test(N);
+        System.out.println(N + " is a perfect square? " + result2);
+
+        N = 10000;
+        boolean result3 = isPerfectSquare.test(N);
+        System.out.println(N + " is a perfect square? " + result3);
+    }
+}
