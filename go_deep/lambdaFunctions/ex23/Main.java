b9a967268df4fb11064e00ed69a6f33aab480271
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2025-08-26 13:38:11 +0200

    Add files via upload

---
+++ go_deep/lambdaFunctions/ex23/Main.java
@@ -0,0 +1,21 @@
+package com.example.github.lambdaFunctions.ex23;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List <String> colors = Arrays.asList("Red", "Black", "White", "Orange", "Blue");
+        System.out.println("List of colors: " + colors);
+
+        double averageLength = calculateAverageLength(colors);
+        System.out.println("Average length of colors(strings): " + averageLength);
+    }
+
+    public static double calculateAverageLength(List < String > strings) {
+        return strings.stream()
+                .mapToInt(String::length) // Convert each string to its length
+                .average() // Calculate the average
+                .orElse(0); // If the list is empty, return 0 as the default value
+    }
+}
