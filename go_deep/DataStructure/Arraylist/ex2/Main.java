e07aa19da1376bfa3fb3aaccef10fe7c69ebda88
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2025-10-23 09:17:34 +0200

    Add files via upload

---
+++ go_deep/DataStructure/Arraylist/ex2/Main.java
@@ -0,0 +1,23 @@
+package com.example.github.DataStructure.Arraylist.ex2;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Creae a list and add some colors to the list
+        List<String> list_Strings = new ArrayList<String>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+        // Print the list
+        for (String element : list_Strings) {
+            System.out.println(element);
+        }
+    }
+}
+
